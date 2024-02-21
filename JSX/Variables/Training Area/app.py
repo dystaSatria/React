917948d0b6298b1a1c9be96c37e051fe21198717
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2024-02-21 04:23:20 +0300

    Create app.py

---
+++ JSX/Variables/Training Area/app.py
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container);
+const goose = 'https://content.codecademy.com/courses/React/react_photo-goose.jpg';
+const gooseImg = <img src={goose} />;
+root.render(gooseImg);
