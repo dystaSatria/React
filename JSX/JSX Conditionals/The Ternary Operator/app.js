d88915d4c64b2caa69dd242d838617cb6c606dc3
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2024-02-21 07:48:01 +0300

    Create app.js

---
+++ JSX/JSX Conditionals/The Ternary Operator/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+function coinToss() {
+  
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
+
+root.render(img);
